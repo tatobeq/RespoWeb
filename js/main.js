b32826fad23b1888de884d693add68660b332df5
tato <tato.beq@gmail.com>
2016-03-23 02:09:56 +0400

    Social Icons added

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$( ".facebook" ).mouseover(function() {
+  $(".st0").css("stroke", "#0f3e9f");
+});
+$( ".facebook" ).mouseleave(function() {
+  $(".st0").css("stroke", "#777");
+});
+
+$( ".twitter" ).mouseover(function() {
+  $(".st2").css("stroke", "#4c86fe");
+});
+$( ".twitter" ).mouseleave(function() {
+  $(".st2").css("stroke", "#777");
+});
